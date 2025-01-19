7c127c2be8aeb3915988f4d845728cddecf86bfb
Kevin Norman <spaxtersurvival@gmail.com>
2025-01-19 20:50:52 +0100

    first commit

---
+++ src/main/java/dev/spaxter/pixeltasktypes/PixelTaskTypes.java
@@ -0,0 +1,16 @@
+package dev.spaxter.pixeltasktypes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PixelTaskTypes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
