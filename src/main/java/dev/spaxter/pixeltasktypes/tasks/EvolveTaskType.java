8413ecf83b33307e08caef170bf35dc1d20579ea
Kevin Norman <spaxtersurvival@gmail.com>
2025-01-31 18:56:18 +0100

    add evolution types config

--- src/main/java/dev/spaxter/pixeltasktypes/tasks/EvolveTaskType.java
+++ src/main/java/dev/spaxter/pixeltasktypes/tasks/EvolveTaskType.java
@@ -9,15 +9,23 @@ import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import dev.spaxter.pixeltasktypes.PixelTaskTypes;
 import dev.spaxter.pixeltasktypes.util.ArclightUtils;
 import dev.spaxter.pixeltasktypes.util.QuestHelper;
+import dev.spaxter.pixeltasktypes.validation.PixelmonTaskConfigValidator;
+import dev.spaxter.pixeltasktypes.validation.ValidationConstants;
+
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EvolveTaskType extends PixelmonTaskType {
-
     public EvolveTaskType(PixelTaskTypes plugin) {
         super(plugin, "evolve_pokemon", "Evolve Pokémon");
 
         super.addConfigValidator(TaskUtils.useRequiredConfigValidator(this, "amount"));
+        super.addConfigValidator(PixelmonTaskConfigValidator.useStringListValidator(
+            ValidationConstants.EVOLUTION_TYPES, this, "evolution_types"));
     }
 
     @SubscribeEvent
@@ -25,18 +33,21 @@ public class EvolveTaskType extends PixelmonTaskType {
         final ServerPlayerEntity player = event.getPlayer();
         final Player bukkitPlayer = ArclightUtils.getBukkitPlayer(player.getUUID());
         final QPlayer questPlayer = this.plugin.getQuestsApi().getPlayerManager().getPlayer(player.getUUID());
-        PixelTaskTypes.logger.info(event.getEvolution().evoType);
+        String evolutionType = event.getEvolution().evoType;
         Pokemon pokemon = event.getPokemon();
-
-        PixelTaskTypes.logger.info("Evolve: " + pokemon.getSpecies().getName());
 
         for (final TaskUtils.PendingTask pendingTask : TaskUtils.getApplicableTasks(bukkitPlayer, questPlayer, this)) {
             final Task task = pendingTask.task();
+
+            List<String> requiredEvolutionTypes = QuestHelper.getConfigStringListAsLowercase(task, "evolution_types");
+
+            if (requiredEvolutionTypes != null && !requiredEvolutionTypes.contains(evolutionType)) {
+                continue;
+            }
 
             if (this.checkPokemon(pokemon, task)) {
                 QuestHelper.incrementNumericProgress(pendingTask);
             }
         }
     }
-
 }
